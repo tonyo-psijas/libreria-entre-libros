bbca721de31d3ea33d2efa7b36ef1cfaf7f9ba75
tonyo-psijas <appsijasig@gmail.com>
2026-04-29 13:21:45 -0400

    seccion hero agregada

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import { libros } from '../mock_data/products'
 import ProductCard from '../components/ProductCard'
+import { HeroComponent } from '../components/HeroComponent'
 
 export const Home = () => {
     return (
-      <div className='container'>
-        <div className="row g-4">
-            {
-            libros.result.map((libro) => (
-                <div
-                key={libro.id_libro}
-                className="
-                  col-12
-                  col-sm-6
-                  col-md-4
-                  col-lg-2
-                "
-              >
-                <ProductCard
-                  imagen={libro.imagen}
-                  id_libro={libro.id_libro}
-                  titulo={libro.titulo}
-                  autor={libro.autor}
-                  precio={libro.precio}
-                />
-              </div>
-            ))
-            }
-        </div>
-      </div>
+        <>
+            <HeroComponent />
+            <div className='container'>
+                <div className="row g-4">
+                    {
+                    libros.result.map((libro) => (
+                        <div
+                        key={libro.id_libro}
+                        className="
+                        col-6
+                        col-sm-4
+                        col-lg-2
+                        "
+                    >
+                        <ProductCard
+                        imagen={libro.imagen}
+                        id_libro={libro.id_libro}
+                        titulo={libro.titulo}
+                        autor={libro.autor}
+                        precio={libro.precio}
+                        />
+                    </div>
+                    ))
+                    }
+                </div>
+            </div>
+        </>
     )
   }
